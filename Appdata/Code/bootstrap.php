fb96c58b6549c29c21e0b1c49d49bf02aab9a133
Wolfiku <133115430+Wolfiku@users.noreply.github.com>
2026-03-14 15:32:01 +0100

    Delete Appdata directory

--- Appdata/Code/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Sandbox: Nur der App-Ordner + /tmp erlaubt
-ini_set('open_basedir', dirname(__DIR__) . ':/tmp/');
-
-// Logging relativ zum App-Ordner
-ini_set('log_errors', '1');
-ini_set('error_log', dirname(__DIR__) . '/logs/php_errors.log');
